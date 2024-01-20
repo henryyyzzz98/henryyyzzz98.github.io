19c52d50efafb6bfe05a8ff3bec889e937f1aea2
henrylai543 <henrylai543@gmail.com>
2024-01-20 13:57:44 +0800

    Update nmixx_songs.js

--- js/nmixx_songs.js
+++ js/nmixx_songs.js
@@ -4,64 +4,52 @@ let songs = [
       "answer": "NMIXX - XOXO"
     },
     {
-      "url": "https://soundcloud.com/nmixx-scmusic/passionfruit",
-      "answer": "NMIXX - Passionfruit"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/break-the-wall",
-      "answer": "NMIXX - Break The Wall"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/dash",
-      "answer": "NMIXX - DASH"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/boom",
-      "answer": "NMIXX - BOOM"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/run-for-roses",
-      "answer": "NMIXX - Run For Roses"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/sonar-breaker",
-      "answer": "NMIXX - Soñar (Breaker)"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/party-oclock",
-      "answer": "NMIXX - Party O'Clock"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/roller-coaster",
-      "answer": "NMIXX - Roller Coaster"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/home",
-      "answer": "NMIXX - HOME"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/paxxword",
-      "answer": "NMIXX - PAXXWORD"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/my-gosh",
-      "answer": "NMIXX - My Gosh"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/love-me-like-this",
-      "answer": "NMIXX - Love Me Like This"
-    },
-    {
-      "url": "https://soundcloud.com/nmixx-scmusic/just-did-it",
-      "answer": "NMIXX - Just Did It"
+      "url": "https://soundcloud.com/nmixx-scmusic/tank",
+      "answer": "NMIXX - TANK"
     },
     {
       "url": "https://soundcloud.com/nmixx-scmusic/young-dumb-stupid",
       "answer": "NMIXX - Young, Dumb, Stupid"
     },
     {
+      "url": "https://soundcloud.com/nmixx-scmusic/dash",
+      "answer": "NMIXX - DASH"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/break-the-wall",
+      "answer": "NMIXX - Break The Wall"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/just-did-it",
+      "answer": "NMIXX - Just Did It"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/party-oclock",
+      "answer": "NMIXX - Party O'Clock"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/home",
+      "answer": "NMIXX - HOME"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/sonar-breaker",
+      "answer": "NMIXX - Soñar (Breaker)"
+    },
+    {
       "url": "https://soundcloud.com/nmixx-scmusic/funky-glitter-christmas",
       "answer": "NMIXX - Funky Glitter Christmas"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/my-gosh",
+      "answer": "NMIXX - My Gosh"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/o-o",
+      "answer": "NMIXX - O.O"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/roller-coaster",
+      "answer": "NMIXX - Roller Coaster"
     },
     {
       "url": "https://soundcloud.com/nmixx-scmusic/dice",
@@ -72,11 +60,23 @@ let songs = [
       "answer": "NMIXX - COOL (Your rainbow)"
     },
     {
-      "url": "https://soundcloud.com/nmixx-scmusic/o-o",
-      "answer": "NMIXX - O.O"
+      "url": "https://soundcloud.com/nmixx-scmusic/boom",
+      "answer": "NMIXX - BOOM"
     },
     {
-      "url": "https://soundcloud.com/nmixx-scmusic/tank",
-      "answer": "NMIXX - TANK"
+      "url": "https://soundcloud.com/nmixx-scmusic/run-for-roses",
+      "answer": "NMIXX - Run For Roses"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/paxxword",
+      "answer": "NMIXX - PAXXWORD"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/passionfruit",
+      "answer": "NMIXX - Passionfruit"
+    },
+    {
+      "url": "https://soundcloud.com/nmixx-scmusic/love-me-like-this",
+      "answer": "NMIXX - Love Me Like This"
     }
 ];
