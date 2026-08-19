7d2b535de2875782f5d330ae6aefa84ead02ebc9
henryyyzzz98 <henrylai543@gmail.com>
2026-08-19 20:34:58 +0800

    minor fix

--- js/dondcustom20.js
+++ js/dondcustom20.js
@@ -384,7 +384,7 @@ function smartRound(value) {
     return Math.round(value / 100) * 100;
   }
 
-  return Math.round(value / 1000) * 1000;
+  return Math.round(value / 100) * 100;
 }
 
 /* =========================================================
